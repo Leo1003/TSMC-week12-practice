a3bba95496119fe4f9a521744174a44f3932a270
Leo <leo881003@gmail.com>
2022-05-04 19:57:10 +0800

    Refactor unit test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,17 +1,30 @@
 import { Game } from './game';
 
-test('gutter game', () => {
-  const game = new Game();
-  for (let i = 0; i < 20; i++) {
-    game.roll(0);
+describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should create an instance', () => {
+    expect(game).toBeTruthy();
+  });
+
+  test('all zero', () => {
+    rollMany(20, 0);
+    expect(game.score).toBe(0);
+  });
+
+  test('all one', () => {
+    rollMany(20, 1);
+    expect(game.score).toBe(20);
+  });
+
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
   }
-  expect(game.score).toBe(0);
 });
 
-test('all one', () => {
-  const game = new Game();
-  for (let i = 0; i < 20; i++) {
-    game.roll(1);
-  }
-  expect(game.score).toBe(20);
-});
