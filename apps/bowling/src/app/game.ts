c8810a88fdc1181d9df895eee09061b723dc041f
Leo <leo881003@gmail.com>
2022-05-04 21:09:58 +0800

    Refactor whole codes

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,47 +1,71 @@
 export class Game {
-  private rolls: number[] = [];
+  private rolls: number[] = new Array(23).fill(0);
   private currentRoll = 0;
 
   roll(pins: number) {
-    this.rolls[this.currentRoll++] = pins;
+    if (!Number.isInteger(pins) || pins < 0 || pins > 10) {
+      throw new RangeError('score should be an integer within 0 ~ 10');
+    }
+    if (this.currentRoll < 20 || (this.isStrike(9) && this.currentRoll < 23)) {
+      this.rolls[this.currentRoll++] = pins;
+
+      // Skip to next roll if this is a strike
+      if (this.isStrike(Math.trunc(this.currentRoll / 2))) {
+        this.currentRoll++
+      }
+    }
   }
 
   get score() {
     let score = 0;
-    let frameIndex = 0;
 
     for (let frame = 0; frame < 10; frame++) {
-      if (this.isStrike(frameIndex)) {
-        score += 10 + this.strikeBonus(frameIndex);
-        frameIndex++;
-        continue;
-      }
-
-      if (this.isSpare(frameIndex)) {
-        score += 10 + this.spareBonus(frameIndex);
-      } else {
-        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
-      }
-
-      frameIndex += 2;
+      score += this.frameScore(frame);
     }
 
     return score;
   }
 
-  private isStrike(frameIndex: number) {
-    return this.rolls[frameIndex] === 10;
+  private frameScore(frame: number): number {
+    if (this.isStrike(frame)) {
+      return this.rawFrameScore(frame) + this.strikeBonus(frame);
+    } else if (this.isSpare(frame)) {
+      return this.rawFrameScore(frame) + this.spareBonus(frame);
+    } else {
+      return this.rawFrameScore(frame);
+    }
   }
 
-  private isSpare(frameIndex: number) {
-    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+  private rawFrameScore(frame: number): number {
+    let r = frame * 2;
+    let score1 = this.rolls[r];
+    let score2 = this.rolls[r + 1];
+
+    return Math.min(score1 + score2, 10);
   }
 
-  private strikeBonus(frameIndex: number) {
-    return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+  private frameFirst(frame: number): number {
+    let r = frame * 2;
+    return this.rolls[r];
   }
 
-  private spareBonus(frameIndex: number) {
-    return this.rolls[frameIndex + 2];
+  private isStrike(frame: number): boolean {
+    return this.frameFirst(frame) === 10;
+  }
+
+  private isSpare(frame: number): boolean {
+    return this.rawFrameScore(frame) === 10;
+  }
+
+  private strikeBonus(frame: number): number {
+    if (this.isStrike(frame + 1)) {
+      return this.rawFrameScore(frame + 1) + this.frameFirst(frame + 2);
+    } else {
+      return this.rawFrameScore(frame + 1)
+    }
+  }
+
+  private spareBonus(frame: number): number {
+    return this.frameFirst(frame + 1);
   }
 }
