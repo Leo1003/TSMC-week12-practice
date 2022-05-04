ebca45a6df8ea9226bbae11273c0bb2acf1f2e7c
Leo <leo881003@gmail.com>
2022-05-04 19:58:08 +0800

    Add spare test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -21,6 +21,14 @@ describe('Game', () => {
     expect(game.score).toBe(20);
   });
 
+  test('test on one spare', () => {
+    game.roll(5);
+    game.roll(5); // spare
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score).toBe(16);
+  });
+
   function rollMany(n: number, pins: number) {
     for (let i = 0; i < n; i++) {
       game.roll(pins);
