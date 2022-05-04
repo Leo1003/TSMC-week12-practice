aa19fda3dd3dc893c6323c72761d7cf6dcd24b2b
Leo <leo881003@gmail.com>
2022-05-04 19:53:53 +0800

    Add new test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -7,3 +7,11 @@ test('gutter game', () => {
   }
   expect(game.score).toBe(0);
 });
+
+test('all one', () => {
+  const game = new Game();
+  for (let i = 0; i < 20; i++) {
+    game.roll(1);
+  }
+  expect(game.score).toBe(20);
+});
