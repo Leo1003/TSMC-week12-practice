f6cf5f6365b8b9037b01eaf149827a1dd7c5fe38
Leo <leo881003@gmail.com>
2022-05-04 20:01:47 +0800

    Refactor codes

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -8,15 +8,20 @@ export class Game {
 
   get score() {
     let score = 0;
-    let i = 0;
+    let frameIndex = 0;
+
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[i] + this.rolls[i + 1] === 10) {
-        score += 10 + this.rolls[i + 2];
+      if (this.isSpare(frameIndex)) {
+        score += 10 + this.rolls[frameIndex + 2];
       } else {
-        score += this.rolls[i] + this.rolls[i + 1];
+        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
       }
-      i += 2;
+      frameIndex += 2;
     }
     return score;
+  }
+
+  private isSpare(frameIndex: number) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
   }
 }
