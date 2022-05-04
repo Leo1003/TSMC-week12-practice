c50bc35dfcf3813f37a5113cddb85383f1744e6e
Leo <leo881003@gmail.com>
2022-05-04 20:01:04 +0800

    Handle spare condition

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,11 +1,22 @@
 export class Game {
-  private _score = 0;
+  private rolls: number[] = [];
+  private currentRoll = 0;
 
   roll(pins: number) {
-    this._score += pins;
+    this.rolls[this.currentRoll++] = pins;
   }
 
   get score() {
-    return this._score;
+    let score = 0;
+    let i = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[i] + this.rolls[i + 1] === 10) {
+        score += 10 + this.rolls[i + 2];
+      } else {
+        score += this.rolls[i] + this.rolls[i + 1];
+      }
+      i += 2;
+    }
+    return score;
   }
 }
