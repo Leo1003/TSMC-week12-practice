0f08a770f9bd45ff03b31658b549bb0a8eda741a
Leo <leo881003@gmail.com>
2022-05-04 20:10:11 +0800

    Add perfect game test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -37,6 +37,11 @@ describe('Game', () => {
     expect(game.score).toBe(24);
   });
 
+  test('perfect game', () => {
+    rollMany(12, 10);
+    expect(game.score).toBe(300);
+  });
+
   function rollMany(n: number, pins: number) {
     for (let i = 0; i < n; i++) {
       game.roll(pins);
